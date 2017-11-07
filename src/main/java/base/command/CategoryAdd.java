9abb83794759d8fa77e6dfbf4c47ee7c49e2b526
Pasi Junttila <pasi.junttila@live.com>
2017-11-07 14:27:22 +0200

    Category tests, rest of them.

---
+++ src/main/java/base/command/CategoryAdd.java
@@ -0,0 +1,48 @@
+package base.command;
+
+public class CategoryAdd {
+
+    private String name;
+
+    public CategoryAdd() {
+    }
+
+    public CategoryAdd(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        CategoryAdd other = (CategoryAdd) obj;
+        if (name == null) {
+            if (other.name != null) return false;
+        }
+        else if (!name.equals(other.name)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryAdd [name=" + name + "]";
+    }
+
+}
--- src/test/java/base/controller/CategoryControllerTest.java
+++ src/test/java/base/controller/CategoryControllerTest.java
@@ -1,14 +1,16 @@
 package base.controller;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +23,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import base.command.CategoryAdd;
+import base.command.GlassAdd;
 import base.domain.Category;
+import base.dto.CategoryDto;
 import base.repository.CategoryRepository;
 
 @RunWith(SpringRunner.class)
@@ -68,4 +75,81 @@ public class CategoryControllerTest {
         assertFalse("Project list must not be empty.", content.equals("[]"));
     }
 
+    @Test
+    public void addingCategoryReturnsLocationHeaderAndDto() throws Exception {
+        CategoryAdd catAdd = new CategoryAdd(CATEGORY3);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(catAdd);
+        MvcResult result = mockMvc.perform(post(PATH).contentType(MediaType.APPLICATION_JSON_UTF8).content(content))
+                .andExpect(status().isCreated()).andExpect(header().string("Location", containsString(PATH + "/")))
+                .andReturn();
+        CategoryDto dto = mapper.readValue(result.getResponse().getContentAsString(), CategoryDto.class);
+        assertTrue("Category name not correct", dto.getName().equals(CATEGORY3));
+    }
+
+    @Test
+    public void addingCategoryWithoutNameFails() throws Exception {
+        CategoryAdd catAdd = new CategoryAdd(CATEGORY3);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(catAdd);
+        mockMvc.perform(post(PATH).contentType(MediaType.APPLICATION_JSON_UTF8).content(content))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void fetchSingleCategoryOK() throws Exception {
+        Category cat = categoryRepository.findOne(this.id1);
+        String name = cat.getName();
+        MvcResult result = mockMvc.perform(get(PATH + "/" + this.id1)).andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8)).andReturn();
+        assertFalse("Contents cannot be empty!", result.getResponse().getContentAsString().isEmpty());
+        ObjectMapper mapper = new ObjectMapper();
+        CategoryDto dto = mapper.readValue(result.getResponse().getContentAsString(), CategoryDto.class);
+        assertTrue("Category name not correct", dto.getName().equals(name));
+    }
+
+    @Test
+    public void fetchSingleCategoryWithWrongIdFails404() throws Exception {
+        mockMvc.perform(get(PATH + "/" + 751130)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void deleteCategoryOK() throws Exception {
+        mockMvc.perform(delete(PATH + "/" + this.id1)).andExpect(status().isOk());
+    }
+
+    @Test
+    public void deleteCategoryWithWrongIdFails404() throws Exception {
+        mockMvc.perform(delete(PATH + "/" + 751130)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void modifyCategoryOKandReturnsChangedContent() throws Exception {
+        CategoryAdd catAdd = new CategoryAdd(CATEGORY3);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(catAdd);
+        MvcResult result = mockMvc
+                .perform(put(PATH + "/" + this.id2).contentType(MediaType.APPLICATION_JSON_UTF8).content(content))
+                .andExpect(status().isOk()).andReturn();
+        CategoryDto dto = mapper.readValue(result.getResponse().getContentAsString(), CategoryDto.class);
+        assertTrue(dto.getName().equals(CATEGORY3));
+    }
+
+    @Test
+    public void modifyingCategoryWithWrongIdFails404() throws Exception {
+        CategoryAdd catAdd = new CategoryAdd(CATEGORY3);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(catAdd);
+        mockMvc.perform(put(PATH + "/" + 751130).contentType(MediaType.APPLICATION_JSON_UTF8).content(content))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void modifyingCategoryWithEmptyContentsFails() throws Exception {
+        CategoryAdd catAdd = new CategoryAdd(EMPTY_STRING);
+        ObjectMapper mapper = new ObjectMapper();
+        String content = mapper.writeValueAsString(catAdd);
+        mockMvc.perform(put(PATH + "/" + this.id2).contentType(MediaType.APPLICATION_JSON_UTF8).content(content))
+                .andExpect(status().isBadRequest());
+    }
 }
