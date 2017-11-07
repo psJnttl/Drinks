bceba41974224325d9b3633b3963db95f67c5e2b
Pasi Junttila <pasi.junttila@live.com>
2017-11-07 08:40:15 +0200

    Ingredient deletion UI, almost ready.

--- src/main/js/components/Ingredients.js
+++ src/main/js/components/Ingredients.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SimpleInformationModal from './SimpleInformationModal';
+import SimpleConfirmationModal from './SimpleConfirmationModal';
 import axios from 'axios';
 import _ from 'lodash';
-import {Button, Pagination, Table} from 'react-bootstrap';
+import {Button, Glyphicon, Pagination, Table} from 'react-bootstrap';
+
 
 class Ingredients extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {ingredients: [], infoModalOpen: false, }
+    this.state = {ingredients: [], infoModalOpen: false, addModalVisible:false,
+      delConfirmationVisible: false, ingredient: {}}
     this.setIngredientList = this.setIngredientList.bind(this);
     this.closeInfoModal = this.closeInfoModal.bind(this);
+    this.toggleAddModal = this.toggleAddModal.bind(this);
+    this.setDeleteConfirmModalVisible = this.setDeleteConfirmModalVisible.bind(this);
+    this.deleteReply = this.deleteReply.bind(this);
   }
 
   setIngredientList(data) {
@@ -18,8 +24,22 @@ class Ingredients extends React.Component {
        _.assign({}, {id: item.id, name: item.name}) );
     this.setState({ingredients: theList});
   }
+
   closeInfoModal() {
     this.setState({infoModalOpen: false});
+  }
+
+  toggleAddModal() {
+    this.setState({addModalVisible: !this.state.addModalVisible});
+  }
+
+  setDeleteConfirmModalVisible(item) {
+    if (false === this.state.delConfirmationVisible) {
+      this.setState({delConfirmationVisible: true, ingredient: item});
+    }
+  }
+  deleteReply(answer) {
+    this.setState({delConfirmationVisible: false, ingredient: {} });
   }
   componentDidMount() {
     const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
@@ -32,11 +52,16 @@ class Ingredients extends React.Component {
           this.setState({infoModalOpen: true});
         });
   }
+
   render() {
-    const dataRows = this.state.ingredients.map( (item, index) =>
-      <tr>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
+    const dataRows = this.state.ingredients.map( (row, index) =>
+      <tr key={index}>
+        <td>{row.id}</td>
+        <td>{row.name}</td>
+        <td>
+          <Button bsStyle="danger" bsSize="small" onClick={() => this.setDeleteConfirmModalVisible(row)} title="delete"><Glyphicon glyph="trash"/></Button>
+
+        </td>
       </tr>
     );
     return (
@@ -49,11 +74,23 @@ class Ingredients extends React.Component {
           notification2 = {this.state.notification2}
           name=""
           reply={this.closeInfoModal} />
+
+        <SimpleConfirmationModal
+          modalOpen={this.state.delConfirmationVisible}
+          title="Delete ingredient"
+          question={"Are you sure you want to delete " + this.state.ingredient.name}
+          reply={this.deleteReply}
+          header="failedModalHeader"
+        />
+
+        <Button bsStyle="success" onClick={ () => this.toggleAddModal() } title="add"><Glyphicon glyph="plus"/></Button>
+
         <Table bordered condensed hover>
           <thead>
             <tr>
               <th>id</th>
               <th>name</th>
+              <th>action</th>
             </tr>
           </thead>
           <tbody>
@@ -63,6 +100,20 @@ class Ingredients extends React.Component {
       </div>
     );
   }
+
+  deleteIngredient(ingredient) {
+    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    const self = this;
+    const url = 'api/ingredients/' + ingredient.id;
+    axios.delete(ulr, config)
+         .then(function (response) {
+              console.log("delete ingredient ok");
+         })
+        .catch(function (response) {
+          console.log("delete ingredient failed");
+        });
+  }
+
 }
 Ingredients.PropTypes = {}
 Ingredients.defaultProps = {}
