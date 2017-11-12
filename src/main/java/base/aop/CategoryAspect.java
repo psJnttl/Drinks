a8bf0bfd9ae1640381b7bf64b6961b9c4bb15c0a
Pasi Junttila <pasi.junttila@live.com>
2017-11-12 13:45:31 +0200

    Logging to db Category CUD with AOP.

---
+++ src/main/java/base/aop/CategoryAspect.java
@@ -0,0 +1,84 @@
+package base.aop;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import base.command.CategoryAdd;
+import base.domain.Category;
+import base.domain.LogEntry;
+import base.dto.CategoryDto;
+import base.repository.CategoryRepository;
+import base.repository.LogEntryRepository;
+
+@Aspect
+public class CategoryAspect {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
+    @Autowired
+    private LogEntryRepository logEntryRepository;
+
+    @Before("execution(* base.service.CategoryService.deleteCategory(..))")
+    private void deleteCategory(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        long id = (long) joinPoint.getArgs()[0];
+        Category cat = categoryRepository.findOne(id);
+        LogEntry logEntry = LogEntry.builder()
+                .date(LocalDateTime.now())
+                .username(username)
+                .action("DELETE")
+                .targetEntity("category")
+                .targetId(id)
+                .targetName(cat.getName())
+                .build();
+        logEntryRepository.save(logEntry);
+    }
+    
+    @AfterReturning( pointcut = "execution(* base.service.CategoryService.addCategory(..))",
+            returning = "result")
+    private void addCategory(JoinPoint joinPoint, Object result) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<CategoryDto> category = (Optional<CategoryDto>) result;
+        if (category.isPresent()) {
+            CategoryDto dto = category.get();
+            LogEntry logEntry = LogEntry.builder()
+                    .date(LocalDateTime.now())
+                    .username(username)
+                    .action("CREATE")
+                    .targetEntity("category")
+                    .targetId(dto.getId())
+                    .targetName(dto.getName())
+                    .build();
+            logEntryRepository.save(logEntry);
+        }
+    }
+
+    @Before("execution(* base.service.CategoryService.modifyCategory(..))")
+    private void modifyCategory(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        long id = (long) joinPoint.getArgs()[0];
+        CategoryAdd categoryMod = (CategoryAdd) joinPoint.getArgs()[1];
+        LogEntry after = LogEntry.builder()
+                .date(LocalDateTime.now())
+                .username(username)
+                .action("MODIFY")
+                .targetEntity("category")
+                .targetId(id)
+                .targetName(categoryMod.getName())
+                .build();
+        logEntryRepository.save(after);
+    }
+
+}
