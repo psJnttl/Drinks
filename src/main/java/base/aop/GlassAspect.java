66b9cdadb6a81805e1bfe03a26cc505bc47fe379
Pasi Junttila <pasi.junttila@live.com>
2017-11-12 13:14:05 +0200

    Modify and delete glass logging with AOP to db.

--- src/main/java/base/aop/GlassAspect.java
+++ src/main/java/base/aop/GlassAspect.java
@@ -1,18 +1,23 @@
 package base.aop;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import base.command.GlassAdd;
 import base.domain.Glass;
 import base.domain.LogEntry;
+import base.dto.GlassDto;
 import base.repository.GlassRepository;
 import base.repository.LogEntryRepository;
 
@@ -44,4 +49,43 @@ public class GlassAspect {
                 .build();
         logEntryRepository.save(logEntry); // transactional?
     }
+    
+    @AfterReturning( pointcut = "execution(* base.service.GlassService.addGlass(..))",
+                     returning = "result")
+    private void addGlass(JoinPoint joinPoint, Object result) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        Optional<GlassDto> glass = (Optional<GlassDto>) result;
+        if (glass.isPresent()) {
+            GlassDto dto = glass.get();
+            LogEntry logEntry = LogEntry.builder()
+                    .date(LocalDateTime.now())
+                    .username(username)
+                    .action("CREATE")
+                    .targetEntity("glass")
+                    .targetId(dto.getId())
+                    .targetName(dto.getName())
+                    .build();
+            logEntryRepository.save(logEntry);
+        }
+    }
+    
+    @Before("execution(* base.service.GlassService.modifyGlass(..))")
+    private void modifyGlass(JoinPoint joinPoint) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        long id = (long) joinPoint.getArgs()[0];
+        GlassAdd glassMod = (GlassAdd) joinPoint.getArgs()[1];
+        LocalDateTime now = LocalDateTime.now();
+        LogEntry after = LogEntry.builder()
+                .date(now)
+                .username(username)
+                .action("MODIFY")
+                .targetEntity("glass")
+                .targetId(id)
+                .targetName(glassMod.getName())
+                .build();
+        logEntryRepository.save(after);
+    }
+    
 }
