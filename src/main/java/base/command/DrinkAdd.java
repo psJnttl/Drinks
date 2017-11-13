86be3fba4f8f37159a0d2176241cd28ac71ecba5
Pasi Junttila <pasi.junttila@live.com>
2017-11-13 15:55:25 +0200

    Add cmd for Drink, DTO amended.

--- src/main/java/base/command/DrinkAdd.java
+++ src/main/java/base/command/DrinkAdd.java
@@ -1,33 +1,25 @@
-package base.dto;
+package base.command;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DrinkDto {
+import base.dto.CategoryDto;
+import base.dto.DrinkComponent;
+import base.dto.GlassDto;
 
-    private Long id;
+public class DrinkAdd {
+
     private String name;
     private CategoryDto category;
     private GlassDto glass;
     private List<DrinkComponent> components = new ArrayList<>();
 
-    public DrinkDto() {
+    public DrinkAdd() {
     }
 
-    public DrinkDto(Long id, String name, CategoryDto category, GlassDto glass, List<DrinkComponent> components) {
-        this.id = id;
+    public DrinkAdd(String name) {
         this.name = name;
-        this.category = category;
-        this.glass = glass;
-        this.components = components;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -69,7 +61,6 @@ public class DrinkDto {
         result = prime * result + ((category == null) ? 0 : category.hashCode());
         result = prime * result + ((components == null) ? 0 : components.hashCode());
         result = prime * result + ((glass == null) ? 0 : glass.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
@@ -79,7 +70,7 @@ public class DrinkDto {
         if (this == obj) return true;
         if (obj == null) return false;
         if (getClass() != obj.getClass()) return false;
-        DrinkDto other = (DrinkDto) obj;
+        DrinkAdd other = (DrinkAdd) obj;
         if (category == null) {
             if (other.category != null) return false;
         }
@@ -92,16 +83,11 @@ public class DrinkDto {
             if (other.glass != null) return false;
         }
         else if (!glass.equals(other.glass)) return false;
-        if (id == null) {
-            if (other.id != null) return false;
-        }
-        else if (!id.equals(other.id)) return false;
         if (name == null) {
             if (other.name != null) return false;
         }
         else if (!name.equals(other.name)) return false;
         return true;
     }
-    
-    
+
 }
