56ae1cdc8f399c1f9b312c754f3d235b59842765
Pasi Junttila <pasi.junttila@live.com>
2017-11-14 13:30:00 +0200

    Fetch drinks upon mounting and store to state.

--- src/main/js/components/Drinks.js
+++ src/main/js/components/Drinks.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 class Drinks extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {drinks: [], infoModalVisible: false,  };
+    this.fetchDrinks = this.fetchDrinks.bind(this);
+    this.setDrinkList = this.setDrinkList.bind(this);
   }
 
+  fetchDrinks() {
+    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    const self = this;
+    axios.get('api/drinks', config)
+         .then(function (response) {
+            self.setDrinkList(response.data);
+         })
+         .catch(function (response) {
+            self.setState({infoModalVisible: true});
+         });
+  }
+
+  setDrinkList(data) {
+    const theList = data.map( item =>
+       _.assign({}, {
+         id: item.id,
+         name: item.name,
+         category: item.category,
+         glass: item.glass,
+         components: item.components
+       }) );
+    this.setState({drinks: theList});
+  }
+
+  componentDidMount() {
+    this.fetchDrinks();
+  }
   render() {
     return (
       <h4>Drinks listed here</h4>
