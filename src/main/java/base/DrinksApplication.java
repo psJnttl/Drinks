9fa2aca4521b1295b47c27ede1254d6792560c68
Pasi Junttila <pasi.junttila@live.com>
2017-11-17 09:36:35 +0200

    Redirect from port 8080 to 8443.

--- src/main/java/base/DrinksApplication.java
+++ src/main/java/base/DrinksApplication.java
@@ -1,7 +1,15 @@
 package base;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DrinksApplication {
@@ -9,4 +17,39 @@ public class DrinksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DrinksApplication.class, args);
 	}
+	
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+      TomcatEmbeddedServletContainerFactory tomcat =
+          new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+              SecurityConstraint securityConstraint = new SecurityConstraint();
+              securityConstraint.setUserConstraint("CONFIDENTIAL");
+              SecurityCollection collection = new SecurityCollection();
+              collection.addPattern("/*");
+              securityConstraint.addCollection(collection);
+              context.addConstraint(securityConstraint);
+            }
+          };
+      tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+      return tomcat;
+    }
+
+    @Value("${server.port-http}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
+    private Connector createHttpConnector() {
+      Connector connector =
+          new Connector("org.apache.coyote.http11.Http11NioProtocol");
+      connector.setScheme("http");
+      connector.setSecure(false);
+      connector.setPort(serverPortHttp);
+      connector.setRedirectPort(serverPortHttps);
+      return connector;
+    }
 }
