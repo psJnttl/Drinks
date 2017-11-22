c00bd150dd827753f910d3055408184856676bf3
Pasi Junttila <pasi.junttila@live.com>
2017-11-22 14:44:45 +0200

    User modal: Select and Button for Role selection

--- src/main/js/components/UserModal.js
+++ src/main/js/components/UserModal.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import {Button, Col, Form, FormControl, FormGroup, Modal} from 'react-bootstrap';
+import {Button, Col, Form, FormControl, FormGroup, Glyphicon, Modal, Table} from 'react-bootstrap';
+import SelectEntity from './SelectEntity';
+import UserRoles from './UserRoles';
 
 class UserModal extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {user: {username: "", password: "", roles: []},
+    this.state = {user: {username: "", password: "", roles: []}, role: {},
     }
     this.isFormValid = this.isFormValid.bind(this);
     this.onChangeUsername = this.onChangeUsername.bind(this);
     this.onChangePassword = this.onChangePassword.bind(this);
+    this.handleSelectRole = this.handleSelectRole.bind(this);
+    this.handleAddRole = this.handleAddRole.bind(this);
   }
 
   isFormValid() {
@@ -27,6 +31,21 @@ class UserModal extends React.Component {
     const password = e.target.value;
     const user = _.assign({}, this.state.user, {password: password});
     this.setState({user: user});
+  }
+
+  handleSelectRole(e) {
+    const id = parseInt(e.target.value);
+    const roleIndex = _.findIndex(this.props.roles, (r) => (r.id === id) );
+    const role = _.assign({}, this.props.roles[roleIndex]);
+    this.setState({role: role});
+  }
+
+  handleAddRole() {
+    if (undefined !== this.state.role) {
+      // check for duplicate: a role must only appear once in the list
+      const oldRoles = this.state.user.roles;
+    }
+    return;
   }
 
   componentDidMount() {
@@ -50,7 +69,29 @@ class UserModal extends React.Component {
             <FormGroup controlId="formInput">
               <FormControl type="password" placeholder="password" onChange={this.onChangePassword} value={this.state.user.password} />
             </FormGroup>
-
+            <FormGroup controlId="formSelect">
+              <Table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <SelectEntity
+                        title="Role"
+                        entityList={this.props.roles}
+                        onSelect={this.handleSelectRole}
+                        value={this.state.role}
+                      />
+                    </td>
+                    <td>
+                      <Button bsStyle="success" onClick={ () => this.handleAddRole() } title="add role"><Glyphicon glyph="plus"/></Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+              <UserRoles
+                roles={this.state.user.roles}
+                delete={this.deleteRole}
+              />
+            </FormGroup>
           </Modal.Body>
           <Modal.Footer bsClass="modalFooter">
             <Button bsStyle="success" onClick={ () => this.props.save(this.state.user) }
