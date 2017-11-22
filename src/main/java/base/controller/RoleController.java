69e57511e9474c49d3527360a2015b084eef3801
Pasi Junttila <pasi.junttila@live.com>
2017-11-22 14:12:21 +0200

    List of roles can now be fetched from server (admin only).

---
+++ src/main/java/base/controller/RoleController.java
@@ -0,0 +1,30 @@
+package base.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import base.domain.Role;
+import base.dto.RoleDto;
+import base.repository.RoleRepository;
+
+@RestController
+public class RoleController {
+
+    @Autowired
+    private RoleRepository roleRepository;
+    
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @RequestMapping(value = "/api/roles", method = RequestMethod.GET)
+    public List<RoleDto> listRoles() {
+        List<Role> list = roleRepository.findAll();
+        return list.stream()
+                   .map(r -> new RoleDto(r.getId(), r.getName()))
+                   .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/base/dto/RoleDto.java
@@ -0,0 +1,55 @@
+package base.dto;
+
+public class RoleDto {
+
+    private long id;
+    private String name;
+    
+    public RoleDto() {
+    }
+    
+    public RoleDto(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (id ^ (id >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        RoleDto other = (RoleDto) obj;
+        if (id != other.id) return false;
+        if (name == null) {
+            if (other.name != null) return false;
+        }
+        else if (!name.equals(other.name)) return false;
+        return true;
+    }
+    
+}
