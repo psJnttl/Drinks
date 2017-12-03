c7f575b7cb7e5a43689028882ee76054c287d730
Pasi Junttila <pasi.junttila@live.com>
2017-12-03 15:26:41 +0200

    NetworkApi modified for CSRF.

--- src/main/js/components/NetworkApi.js
+++ src/main/js/components/NetworkApi.js
@@ -4,50 +4,63 @@ import _ from 'lodash';
 export default  {
 
   get(url) {
+    const self = this;
     const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
     return axios.get(url, config)
                 .then(function (response) {
+                  self.csrfCatchToken(response);
                   return response.data;
                 })
                 .catch(function (error) {
+                  self.csrfCatchToken(error.response);
                   throw error.response;
                 });
   },
   post(url, command) {
-    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    const self = this;
+    const config = this.insertCsrfToken({headers: {'X-Requested-With': 'XMLHttpRequest'}});
     const cmd  =  _.assign({}, command);
     return axios.post(url, cmd, config)
                 .then(function (response) {
+                  self.csrfCatchToken(response);
                    return response.data;
                 })
                 .catch(function (error) {
+                  self.csrfCatchToken(error.response);
                    throw error.response;
                 });
   },
   delete(url1, item) {
-    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    const self = this;
+    const config = this.insertCsrfToken({headers: {'X-Requested-With': 'XMLHttpRequest'}})
     const url = url1 + '/' + item.id;
     return axios.delete(url, config)
                 .then(function (response) {
+                  self.csrfCatchToken(response);
                   return response.data;
                 })
                 .catch(function (error) {
+                  self.csrfCatchToken(error.response);
                   throw error.response;
                 });
   },
   put(url1, command) {
-    const config = {headers: {'X-Requested-With': 'XMLHttpRequest'}};
+    const self = this;
+    const config = this.insertCsrfToken({headers: {'X-Requested-With': 'XMLHttpRequest'}});
     const cmd = _.assign({}, command);
     const url = url1 + '/' + cmd.id;
     return axios.put(url, cmd, config)
                 .then(function (response) {
+                  self.csrfCatchToken(response);
                   return response.data;
                 })
                 .catch(function (error) {
+                  self.csrfCatchToken(error.response);
                   throw error.response;
                 });
   },
   sendLogin(username, password) {
+    const self = this;
     const creds = "Basic " + btoa(username + ":" + password);
     const config = {
       headers: {
@@ -57,25 +70,45 @@ export default  {
     };
     return axios.get('api/accounts', config)
                 .then(function (response) {
+                  self.csrfCatchToken(response);
                   return response.data;
                 })
                 .catch(function (error) {
+                  self.csrfCatchToken(error.response);
                   throw error.response;
                 });
   },
   signup(username, password) {
+    const self = this;
     const command = {username: username, password: password, roles: [{"id":1,"name":"USER"}]};
-    const config = {
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    }
+    const config = this.insertCsrfToken({headers: {'X-Requested-With': 'XMLHttpRequest'}});
     return axios.post('api/accounts/signup', command, config)
          .then(function (response) {
+           self.csrfCatchToken(response);
            return response.data;
          })
          .catch(function (error) {
+           self.csrfCatchToken(error.response);
            throw error.response;
          });
+  },
+  csrfCatchToken(response) {
+    if (undefined !== response.headers) {
+      const headers = response.headers;
+      if (undefined !== headers['x-csrf-header'] && undefined !== headers['x-csrf-token']) {
+        const token = headers['x-csrf-token'];
+        this.csrfToken = token;
+      }
+    }
+  },
+  csrfToken: "",
+  insertCsrfToken(config) {
+    if ("" !== this.csrfToken) {
+      const oldHeaders = _.assign({}, config.headers);
+      const newHeaders = _.assign({}, oldHeaders, {'x-csrf-token': this.csrfToken});
+      const newConfig = _.assign({}, config, {headers: newHeaders});
+      return newConfig;
+    }
+    return config;
   }
 }
